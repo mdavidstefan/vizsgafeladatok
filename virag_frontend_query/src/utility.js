7c0182ab184d2a3d65c9c4b8e0bb9b96a57b495f
mdavidstefan <stef.david.marton@gmail.com>
2025-02-05 11:45:25 +0100

    ingatlaniroda weboldal

--- virag_frontend_query/src/utility.js
+++ virag_frontend_query/src/utility.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const getData = async ({ queryKey }) => {
-    const response = await axios.get(queryKey[1])   
+    const response = await axios.get(queryKey[1])
     return response.data
 }
 
-export const updateData = async (url, data, setState) => {
+export const updateData = async (url, data) => {
     const response = await axios.put(url, data)
-    setState(response.data)
-    return
+    return response.data
+
 }
